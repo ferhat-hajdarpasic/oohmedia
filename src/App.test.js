dc2c652f0d0f1f6b7bc511a71960455d923f7fa4
Fred Hajdarapsic <fred.hajdarpasic@superchoice.com.au>
2020-12-02 01:05:48 +1100

    Completed the unit test.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import { render, findByText, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('search by user id = 3 return 2 results as  required', async () => {
+    render(<App />);
+
+    expect(await findByText(document, 'System Administrator')).toBeVisible();
+
+    const textFieldUserId = document.getElementById('textFieldUserId');
+    expect(textFieldUserId).toBeInTheDocument();
+
+    fireEvent.change(textFieldUserId, { target: { value: '3' } })
+    expect(textFieldUserId.value).toBe('3');
+
+    const buttonShowHierarchy = document.getElementById('buttonShowHierarchy');
+    fireEvent.click(buttonShowHierarchy);
+
+    const hierarchyTable = document.getElementById('hierarchyTable');
+    expect(await findByText(hierarchyTable, 'Emily Employee')).toBeVisible();
+    expect(await findByText(hierarchyTable, 'Trent Trainer')).toBeVisible();
 });
